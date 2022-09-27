a3bd92d76dd86ea45c50e416fc9c304518f86993
bei8 <bezi.zhong@gmail.com>
2022-09-27 21:31:10 +0700

    setup slicing files

---
+++ resources/views/layouts/frontend.blade.php
@@ -0,0 +1,34 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.101.0">
+    <title>Album example · Bootstrap v4.6</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.6/examples/album/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    @include('includes.public.style')
+
+  </head>
+  <body>
+    
+    @include('includes.public.header')
+
+    @yield('content')
+
+    @include('includes.public.footer')
+
+    @include('includes.public.script')
+
+      
+  </body>
+</html>
