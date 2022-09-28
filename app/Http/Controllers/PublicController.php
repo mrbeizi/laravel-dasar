e932a1460e3c2eb38ca5af3c3d06a740d9f3d650
bei8 <bezi.zhong@gmail.com>
2022-09-28 20:59:58 +0700

    update relation one to many

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\Category;
 
 class PublicController extends Controller
 {
@@ -17,5 +18,10 @@ class PublicController extends Controller
     public function show(Post $post)
     {
         return view('public.post.show', compact('post'));
+    }
+
+    public function category(Category $category)
+    {
+        return view('public.category.show', compact('category'));
     }
 }
